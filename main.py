e4320ad01883361fcc7e0f5d0581eac21495a234
antonio <antonioleitefilho02@gmail.com>
2023-04-03 11:19:00 -0300

    Atividade concluída v3

--- main.py
+++ main.py
@@ -4,20 +4,22 @@ if decisao < 1 or decisao > 2:
     print("Grau inválido")
 elif decisao == 1:
     print("A equação é do primeiro grau")
-    a = int(input("Digite o valor de a: "))
-    b = int(input("Digite o valor de b: "))
+    a = float(input("Digite o valor de a: "))
     if a == 0:
       print("Valor de a inválido")
     elif a !=0:
-       print(f"{a}x + {b} = 0")  
+       b = float(input("Digite o valor de b: "))
+       x = -b/a
+       print(f"{a}x + {b} = 0") 
+       print(f"x = {x:.2f}") 
 elif decisao == 2:
    print("A equação é do segundo grau")
-   a = int(input("Digite o valor de a: "))
-   b = int(input("Digite o valor de b: "))
-   c = int(input("Digite o valor de a: "))
+   a = float(input("Digite o valor de a: "))
    if a == 0:
       print("Valor de a inválido")
    elif a !=0:
+       b = float(input("Digite o valor de b: "))
+       c = float(input("Digite o valor de a: "))
        print(f"{a}x² + {b}x + {c} = 0")  
        if (b*b) - 4*a*c < 0:
           print("A equação não possui raízes reais")
